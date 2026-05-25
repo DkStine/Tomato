68c3d8a70901ed466e260f3f9ba97471ecd1f623
Deepak Kumar <dkctcak@gmail.com>
2026-05-25 19:46:46 +0530

    adds frontend with Login and Home pages - Login logic implemented

---
+++ frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+export const authService = "http://localhost:5000";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/* Hardcoding the cliendId for now from auth.env */}
+    <GoogleOAuthProvider clientId="798764972573-a9931o2mk9ao41c07qlj5vteesgiai7o.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>,
+);
